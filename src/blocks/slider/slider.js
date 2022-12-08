8e690bc144fbfad4fef9323c9913a6e3d66d6786
Zaurbek Gergiev <zg.dev.31@gmail.com>
2022-12-08 10:37:16 +0300

    fix promo-card+slider

---
+++ src/blocks/slider/slider.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let slidesArray = document.getElementsByClassName('slider__item');
+
+let i = 0;
+
+function clickWatch() {
+
+	for (;i < slidesArray.length; i++) {
+		console.log(slidesArray[i]);
+		continue;
+	}
+}
