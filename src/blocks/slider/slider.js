f72ffe79e9b3793edb862fdd9db6cc60b325ceee
Zaurbek Gergiev <zg.dev.31@gmail.com>
2022-12-19 09:41:34 +0300

    slider start

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,13 +1,39 @@
 "use strict";
 
-let slidesArray = document.getElementsByClassName('slider__item');
+const slider = document.querySelector('._slider');
+const slides = Array.from(document.querySelectorAll('._slider__item'));
+const btnRight = document.querySelector('._slider__button--right');
+const btnLeft = document.querySelector('._slider__button--left');
 
-let i = 0;
+let visibleSlide = 0;
+const slidesNumber = slides.length;
 
-function clickWatch() {
+const slidesMove = function(slide) {
+	slides.forEach((s, index) => (s.style.transform = `translateX(${((index - slide) * 100)}%)`));
+};
 
-	for (;i < slidesArray.length; i++) {
-		console.log(slidesArray[i]);
-		continue;
-	}
-}
+slidesMove(0);
+
+btnRight.addEventListener('click', function() {
+	if (visibleSlide === slidesNumber - 1) {
+		visibleSlide = 0;
+	} else {
+		visibleSlide++;
+	};
+	slidesMove(visibleSlide);
+
+	console.log('click');
+	console.log(visibleSlide);
+});
+
+btnLeft.addEventListener('click', function() {
+	if (visibleSlide === 0) {
+		visibleSlide = slidesNumber - 1;
+	} else {
+		visibleSlide--;
+	};
+	slidesMove(visibleSlide);
+
+	console.log('click');
+	console.log(visibleSlide);
+});
