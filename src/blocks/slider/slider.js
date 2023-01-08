7d191eab7e89278dae44cf5a4386af7c5a1d235f
Zaurbek Gergiev <zg.dev.31@gmail.com>
2023-01-08 17:01:27 +0300

    clear slider.js, remove several img

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,83 +1,3 @@
-// "use strict";
-
-// const slider = document.querySelector('._slider');
-// const slides = Array.from(document.querySelectorAll('._slider__item'));
-// const btnRight = document.querySelector('._slider__button--right');
-// const btnLeft = document.querySelector('._slider__button--left');
-// const dotsList = document.querySelector('._slider__dot-list');
-// const dot = document.querySelector('._slider__dot');
-
-// let visibleSlide = 0; //задаем начальное значение
-// const slidesNumber = slides.length;
-
-// // выстраиваем слайды в линию, задаем смещение на ширину слайда в зависимости от его индекса
-// const slidesMove = function(slide) {
-// 	slides.forEach((s, index) =>
-// 	(s.style.transform = `translateX(${((index - slide) * 100)}%)`));
-// };
-// slidesMove(0); // задаем первый отображаемый слайд
-
-// // создаем пагинацию в зависимости от колич слайдов
-// const createDots = function() {
-// 	slides.forEach(function(_, index) {
-// 		dotsList.insertAdjacentHTML('beforeend',
-// 		`<button class="slider__dot _slider__dot"
-// 		data-slide="${index}"></button>`)
-// 	});
-// };
-// createDots();
-
-// // упарвление классом '--active' для пагинации
-// const activateDots = function(slide) {
-// 	document.querySelectorAll('._slider__dot').
-// 	forEach(dot => dot.classList.remove('slider__dot--active'));
-
-// 	document.querySelector(`._slider__dot[data-slide="${slide}"]`).
-// 	classList.add('slider__dot--active');
-// }
-// activateDots(visibleSlide);
-
-// // перемещение на след слайд
-// const nextSlide = function() {
-// 	if (visibleSlide === slidesNumber - 1) {
-// 		visibleSlide = 0;// перемещение к первому слайду, если они закончились
-// 	} else {
-// 		visibleSlide++;
-// 	};
-// 	slidesMove(visibleSlide);
-// 	activateDots(visibleSlide);
-// };
-
-// // перемещение на предыдущий слайд
-// const prevSlide = function() {
-// 	if (visibleSlide === 0) {
-// 		visibleSlide = slidesNumber - 1;// перемещение к последнему слайду
-// 	} else {
-// 		visibleSlide--;
-// 	};
-// 	slidesMove(visibleSlide);
-// 	activateDots(visibleSlide);
-// };
-
-// btnRight.addEventListener('click', nextSlide);
-// btnLeft.addEventListener('click', prevSlide);
-
-// // перемещение кнопками впарво/влево
-// slider.addEventListener('keydown', function(e) {
-// 	console.log(e);
-// 	if(e.key === 'ArrowRight') nextSlide();
-// 	if(e.key === 'ArrowLeft') prevSlide();
-// });
-
-// // перемещение по клику на пагинации
-// dotsList.addEventListener('click', function(e) {
-// 	if(e.target.classList.contains('_slider__dot')) {
-// 		const slide = e.target.dataset.slide;
-// 		slidesMove(slide);
-// 		activateDots(slide);
-// 	};
-// };
-
 o2.slider = {
 
 	slider: document.querySelector('._slider'),
@@ -90,31 +10,31 @@ o2.slider = {
 
 
 	// выстраиваем слайды в линию, задаем смещение на ширину слайда в зависимости от его индекса
-	slidesMove (slide) {
+	slidesMove(slide) {
 		this.slides.forEach((s, index) =>
-		(s.style.transform = `translateX(${((index - slide) * 100)}%)`));
+			(s.style.transform = `translateX(${((index - slide) * 100)}%)`));
 	},
 
 	// создаем пагинацию в зависимости от колич слайдов
-	createDots () {
-		this.slides.forEach(function(_, index) {
+	createDots() {
+		this.slides.forEach(function (_, index) {
 			o2.slider.dotsList.insertAdjacentHTML('beforeend',
-			`<button onclick="o2.slider.dotsClick(event)" class="slider__dot _slider__dot"
+				`<button onclick="o2.slider.dotsClick(event)" class="slider__dot _slider__dot"
 			data-slide="${index}"></button>`)
 		});
 	},
 
 	// упарвление классом '--active' для пагинации
-	activateDots (slide) {
+	activateDots(slide) {
 		document.querySelectorAll('._slider__dot').
-		forEach(dot => dot.classList.remove('slider__dot--active'));
+			forEach(dot => dot.classList.remove('slider__dot--active'));
 
 		document.querySelector(`._slider__dot[data-slide="${slide}"]`).
-		classList.add('slider__dot--active');
+			classList.add('slider__dot--active');
 	},
 
 	// перемещение на след слайд
-	nextSlide () {
+	nextSlide() {
 		if (this.visibleSlide === this.slides.length - 1) {
 			this.visibleSlide = 0;// перемещение к первому слайду
 		} else {
@@ -125,7 +45,7 @@ o2.slider = {
 	},
 
 	// перемещение на предыдущий слайд
-	prevSlide () {
+	prevSlide() {
 		if (this.visibleSlide === 0) {
 			this.visibleSlide = this.slides.length - 1;// перемещение к последнему слайду
 		} else {
@@ -136,8 +56,8 @@ o2.slider = {
 	},
 
 	// перемещение по клику на dots
-	dotsClick () {
-		if(event.target.classList.contains('_slider__dot')) {
+	dotsClick() {
+		if (event.target.classList.contains('_slider__dot')) {
 			const slide = +event.target.dataset.slide; // присваиваем значение "slide[index]" пременной и предаем его аргументом след. функциям
 			this.slidesMove(slide);
 			this.activateDots(slide);
@@ -152,20 +72,20 @@ o2.slider = {
 	x2: null,
 
 	// определяем начальную точку касания экрана
-	touchStart () {
+	touchStart() {
 		this.firstTouch = event.changedTouches[0].clientX;
 		x1 = this.firstTouch;
 	},
 	// определяем конечную точку свайпа
-	touchEnd () {
+	touchEnd() {
 		this.progressTouch = event.changedTouches[0].clientX;
 		x2 = this.progressTouch;
 		if (!x1 || !x2) { // если  точек нет -> false
 			false
 		} else {
 			(x2 > x1) ? this.prevSlide()
-			: (x2 < x1) ? this.nextSlide()
-			: false
+				: (x2 < x1) ? this.nextSlide()
+					: false
 		};
 		// обнуляем значения переменных
 		x1 = null;
